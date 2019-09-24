d71acc84b812a894d61cebd484ec02cf9ec67fb6
brennan <brennan@brennan-ubuntu>
2019-09-23 17:34:01 -0700

    updated OVSTest for all inflection conditions

--- WordOrder/src/wordorder/OVSTest.java
+++ WordOrder/src/wordorder/OVSTest.java
@@ -466,6 +466,7 @@ public class OVSTest extends Test {
 						e.probability > 0.0) {
 					switch(i) {
 						case 7: case 9:
+							// verb agrees with the subj (case 7) and the subj&obj (case 9) in 1st/2nd/3rd person
 							if(lexicons.get(lexiconCode.get(conditionCode[i])).get(e.person.toString()).inflection.get("person").get(0) == second.get(conditionCode[i]).inflection.get("subj").get(0)) {
 								factor++;
 								if (e.action.equals(Actions.valueOf(second.get(conditionCode[i]).word.toUpperCase()))) {
@@ -475,6 +476,7 @@ public class OVSTest extends Test {
 							}
 							break;
 						case 10: case 12:
+							// verb agrees with the subj (case 10) and the subj&obj (case 12) in sing/pl
 							if(lexicons.get(lexiconCode.get(conditionCode[i])).get(e.person.toString()).inflection.get("number").get(0) == second.get(conditionCode[i]).inflection.get("subj").get(0)) {
 								factor++;
 								if (e.action.equals(Actions.valueOf(second.get(conditionCode[i]).word.toUpperCase()))) {
@@ -485,6 +487,7 @@ public class OVSTest extends Test {
 							break;
 						case 13: case 14: case 15: case 16: case 17: case 18: case 19:
 						case 27: case 28: case 29: case 30: case 31: case 32: case 33:
+							// verb agrees with the subj (cases 13-19) and the subj&obj (cases 27-33) in 1st/2nd/3rd person & sing/pl
 							if(lexicons.get(lexiconCode.get(conditionCode[i])).get(e.person.toString()).inflection.get("person").get(0) == second.get(conditionCode[i]).inflection.get("subj").get(0) &&
 									lexicons.get(lexiconCode.get(conditionCode[i])).get(e.person.toString()).inflection.get("number").get(0) == second.get(conditionCode[i]).inflection.get("subj").get(1)) {
 								factor++;
@@ -496,8 +499,19 @@ public class OVSTest extends Test {
 							break;
 						case 34: case 35: case 36: case 37: case 38: case 39: case 40:
 						case 41: case 42: case 43: case 44: case 45: case 46: case 47:
-						case 48: case 49: case 50: case 51: case 52:
-						case 60: case 61: case 62: case 63: case 64: case 65: case 66:
+						case 48: case 49: case 50: case 51: case 52: case 53: case 54:
+						case 55: case 56: case 57: case 58: case 59: case 60: case 61:
+						case 62: case 63: case 64: case 65: case 66: case 67: case 68:
+						case 69: case 70: case 71: case 72: case 73: case 74: case 75:
+						case 76: case 77: case 78: case 79: case 80: case 81: case 82:
+						case 132: case 133: case 134: case 135: case 136: case 137: case 138:
+						case 139: case 140: case 141: case 142: case 143: case 144: case 145:
+						case 146: case 147: case 148: case 149: case 150: case 151: case 152:
+						case 153: case 154: case 155: case 156: case 157: case 158: case 159:
+						case 160: case 161: case 162: case 163: case 164: case 165: case 166:
+						case 167: case 168: case 169: case 170: case 171: case 172: case 173:
+						case 174: case 175: case 176: case 177: case 178: case 179: case 180:
+							// verb agrees with the subj (cases 34-82) and the subj&obj (cases 132-180) in noun class
 							if(lexicons.get(lexiconCode.get(conditionCode[i])).get(e.person.toString()).inflection.get("class").get(0) == second.get(conditionCode[i]).inflection.get("subj").get(0)) {
 								factor++;
 								if (e.action.equals(Actions.valueOf(second.get(conditionCode[i]).word.toUpperCase()))) {
@@ -505,13 +519,39 @@ public class OVSTest extends Test {
 									probs.add(e.probability);
 								}
 							}
-							break;					
+							break;
+						case 181: case 182: case 183: case 184: case 185: case 186: case 187:
+						case 188: case 189: case 190: case 191: case 192: case 193: case 194:
+						case 195: case 196: case 197: case 198: case 199: case 200: case 201:
+						case 202: case 203: case 204: case 205: case 206: case 207: case 208:
+						case 209: case 210: case 211: case 212: case 213: case 214: case 215:
+						case 216: case 217: case 218: case 219: case 220: case 221: case 222:
+						case 223: case 224: case 225: case 226: case 227: case 228: case 229:
+						case 279: case 280: case 281: case 282: case 283: case 284: case 285:
+						case 286: case 287: case 288: case 289: case 290: case 291: case 292:
+						case 293: case 294: case 295: case 296: case 297: case 298: case 299:
+						case 300: case 301: case 302: case 303: case 304: case 305: case 306:
+						case 307: case 308: case 309: case 310: case 311: case 312: case 313:
+						case 314: case 315: case 316: case 317: case 318: case 319: case 320:
+						case 321: case 322: case 323: case 324: case 325: case 326: case 327:
+							// verb agrees w/ subj (cases 181-229) and the subj&obj (cases 279-327) in 1st/2nd/3rd per & sing/pl & noun class
+							if(lexicons.get(lexiconCode.get(conditionCode[i])).get(e.person.toString()).inflection.get("person").get(0) == second.get(conditionCode[i]).inflection.get("subj").get(0) &&
+									lexicons.get(lexiconCode.get(conditionCode[i])).get(e.person.toString()).inflection.get("number").get(0) == second.get(conditionCode[i]).inflection.get("subj").get(1) &&
+									lexicons.get(lexiconCode.get(conditionCode[i])).get(e.person.toString()).inflection.get("class").get(0) == second.get(conditionCode[i]).inflection.get("subj").get(2)) {
+								factor++;
+								if (e.action.equals(Actions.valueOf(second.get(conditionCode[i]).word.toUpperCase()))) {
+									// how many of those events could involve both the obj and verb?
+									probs.add(e.probability);
+								}
+							}
+							break;
 						default:
 							factor++;
 							if (e.action.equals(Actions.valueOf(second.get(conditionCode[i]).word.toUpperCase()))) {
 								// how many of those events could involve both the obj and verb?
 								probs.add(e.probability);
 							}
+							break;
 					}
 				}
 			}
